ab6335c1612d059f850a44882de68649050ac5c9
KMIS2008 <129222339+KMIS2008@users.noreply.github.com>
2023-11-04 23:15:18 +0200

    finish1

--- src/components/FormAddContact.js
+++ src/components/FormAddContact.js
@@ -27,8 +27,8 @@ export const FormAddContact = ({onAdd})=> {
                 <FieldStyle            
                      type="text"
                      name="name"
-                    //  pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-                    //  title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+                     pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+                     title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
                      required />
             </FormLabel>
 
@@ -36,7 +36,7 @@ export const FormAddContact = ({onAdd})=> {
                 <FieldStyle            
                      type="tel"
                      name="number"
-                    //  pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+                     pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
                      title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
                      required />
             </FormLabel>
